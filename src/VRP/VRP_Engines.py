b08f8b5c0f0642131541ff8f06c529396cda09ea
toto <rometpierre@gmail.com>
2020-05-18 16:07:20 +0200

    src/VRP
    Include package for VRP feature

---
+++ src/VRP/VRP_Engines.py
@@ -0,0 +1,29 @@
+'''
+Created on 18 mai 2020
+
+@author: promet
+'''
+
+class VRP_Engines:
+    
+    def __init__(self):
+        pass
+    
+    def VRP(self):
+        """
+        Default VRP for test & Debug
+        """
+        pass
+    
+    def VRP_C(self):
+        """
+        Capacitated VRP
+        """
+        pass
+    
+    def VRP_CE(self):
+        """
+        Capacitated & Vehicle Energy VRP
+        """
+        pass
+    
