90fda3ffba8bc9cb5e1dc2aa1b7b189843801970
toto <rometpierre@gmail.com>
2021-04-22 13:12:06 +0200

    Edge: update "__init__" to get dict from db extract

--- src/Graph/Edge.py
+++ src/Graph/Edge.py
@@ -4,7 +4,6 @@ Created on 27 avr. 2020
 @author: promet
 '''
 
-from Graph import Vertex as vtx
 #from Algorithms.VRP.ACO.CommonKnowledge import CommonKnowledge
 
 class Edge:
@@ -13,11 +12,20 @@ class Edge:
     Init object parameters value
     By default, nrj_cost is init at 0,25 kwh/km, according to data exploitation
     """
-    def __init__(self, length_p = 0, vtx_begin_p = vtx, vtx_end_p = vtx, nrj_cost_p = 0.1): #0.25 is for test app. thanks to Romain's data.
-        self.__length       = length_p      #distance Unit: Km
+    def __init__(self, vtx_begin_p = None, vtx_end_p = None, dataDict_p = None  ):
+        #nrj_Wh_p = None, dist_km_p = None, time_min_p = None
+        assert vtx_begin_p  != None, "Empty vtx_begin_p, 'TRUBBLE' empty export from vtxList"
+        assert vtx_end_p    != None, "Empty vtx_end_p, 'TRUBBLE' empty export from vtxList"
+        assert dataDict_p   != None, "Empty dataDict_p, 'TRUBBLE' empty export from database"
+        #assert nrj_Wh_p     == None, "Empty nrj_Wh_p, 'TRUBBLE' empty export from database"
+        #assert dist_km_p    == None, "Empty dist_km_p, 'TRUBBLE' empty export from database"
+        #assert time_min_p   == None, "Empty time_min_p, 'TRUBBLE' empty export from database"
+        
         self.__vtx_begin    = vtx_begin_p
         self.__vtx_end      = vtx_end_p        
-        self.__nrj_cost     = nrj_cost_p    #Nrj unit: KWH/Km
+        self.__nrj_cost     = dataDict_p["NRJ_Wh"]      #Energy unit: kwh/km
+        self.__length       = dataDict_p["Distance_km"] #distance unit: km
+        self.__time         = dataDict_p["time_min"]    #time unit: minute
     
     def set_length(self, length_p):
         """
