b6719b3d688a2aab9c81ddc2f60d35386886fb07
toto <rometpierre@gmail.com>
2021-07-30 12:37:46 +0200

    VRP working - Simulator

--- src/Simulator/main_VRP.py
+++ src/Simulator/main_VRP.py
@@ -9,28 +9,31 @@ from Graph.myGraphs.GpBelfort import GpBelfort
 from Algorithms.VRP.VRP_Engines import VRP_Engines
 from Graph.myGraphs.bdd_Graph.dbExploit import dbExploit
 from Graph.myGraphs.MiddleGraph import MiddleGraph
+from Utilities.DelivListGen import DelivListGen
 from Algorithms.VRP.ACO.CommonKnowledge import CommonKnowledge
 
 '''
 Initialize connection to the graph database
 '''
+
 dbExploit.init_connection()
 
 '''
-Create the graph and initialized the common Knowledge
+Create graph of the public system transport of Belfort 
 '''
-warehouse_vertex = "Gare"
 
-#GpTest.create(warehouse_vertex)
-GpBelfort.create()  #GpBelfort.create(warehouse_vertex)
+GpBelfort.create()
 
 '''
 Create intermediate graph based on delivery points list
 '''
 
-MiddleGraph.create(CommonKnowledge.deliveryList, warehouse_vertex)
+#MiddleGraph.create(DelivListGen.debugList)
+MiddleGraph.create(DelivListGen.run(80, CommonKnowledge.warehouse_vertex))
+
 
 '''
 Start the ECHVRP algorithm
 '''
-VRP_Engines.ESCSHVRP(2)
+
+VRP_Engines.ESCSHVRP(3)
